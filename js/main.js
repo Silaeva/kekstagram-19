9e307e55865873386e2c120a0212aead61656953
Anna <tkachenko481@mail.ru>
2020-01-31 15:14:40 +0300

    Прячет блоки на большой фотографии

--- js/main.js
+++ js/main.js
@@ -109,3 +109,8 @@ var addCommentElements = function (commentData) {
 
 addCommentElements((createComments(COMMENTS_NUMBER)));
 
+document.querySelector('.social__comment-count').classList.add('hidden');
+document.querySelector('.comments-loader').classList.add('hidden');
+
+
+document.querySelector('body').classList.add('modal-open');
