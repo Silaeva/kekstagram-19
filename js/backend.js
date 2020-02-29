e1ed4f058b2554f3ae9c826d1ebd0672c562aa3d
Anna <tkachenko481@mail.ru>
2020-02-29 20:34:32 +0300

    Добавляет отправку формы на сервер

--- js/backend.js
+++ js/backend.js
@@ -2,6 +2,7 @@
 
 (function () {
   var URL_GET = 'https://js.dump.academy/kekstagram/data';
+  var URL_POST = 'https://js.dump.academy/kekstagram';
   var StatusCode = {
     OK: 200
   };
@@ -31,7 +32,32 @@
     xhr.send();
   };
 
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === StatusCode.OK) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = TIMEOUT_IN_MS;
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
   window.backend = {
-    load: load
+    load: load,
+    save: save
   };
 })();
--- js/form.js
+++ js/form.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function () {
+  var formElement = document.querySelector('.img-upload__form');
   var editStartElement = document.querySelector('.img-upload__input');
   var editFormElement = document.querySelector('.img-upload__overlay');
   var editFormCloseElement = editFormElement.querySelector('.img-upload__cancel');
@@ -9,6 +10,7 @@
   var scaleValueElement = editFormElement.querySelector('.scale__control--value');
   var commentInputElement = editFormElement.querySelector('.text__description');
   var hashtagInputElement = editFormElement.querySelector('.text__hashtags');
+  var inputOriginalElement = editFormElement.querySelector('[value="none"]');
 
   var editFormEscPressHandler = function (evt) {
     var active = document.activeElement;
@@ -33,9 +35,13 @@
     editedPhotoElement.style.filter = '';
     editedPhotoElement.style.transform = '';
     scaleValueElement.value = 100 + '%';
+    hashtagInputElement.value = '';
+    commentInputElement.value = '';
+    inputOriginalElement.checked = true;
     document.querySelector('body').classList.remove('modal-open');
     window.editing.removeHandlers();
     window.validation.removeHandlers();
+    formElement.removeEventListener('submit', formSubmitHandler);
   };
 
   editStartElement.addEventListener('change', function (evt) {
@@ -54,4 +60,87 @@
   editFormCloseElement.addEventListener('keydown', function (evt) {
     window.utils.isEnterEvent(evt, closeEditForm);
   });
+
+  //  6.3
+
+  var successTemplateElement = document.querySelector('#success').content.querySelector('.success');
+  var errorTemplateElement = document.querySelector('#error').content.querySelector('.error');
+  var main = document.querySelector('main');
+
+  var createResultMessage = function (template, message) {
+    var messageElement = template.cloneNode(true);
+
+    if (message) {
+      var messageTitle = messageElement.querySelector('.error__title');
+      messageTitle.textContent = message;
+    }
+    main.appendChild(messageElement);
+  };
+
+  var successLayerElement = document.querySelector('.success');
+
+  var successHandler = function () {
+    closeEditForm();
+    createResultMessage(successTemplateElement);
+
+    successLayerElement = document.querySelector('.success');
+    var messageButtonElement = successLayerElement.querySelector('.success__button');
+    var successInnerElement = successLayerElement.querySelector('.success__inner');
+    document.addEventListener('keydown', successMessageEscHandler);
+    messageButtonElement.addEventListener('click', function () {
+      closeSuccessMessage();
+    });
+    successLayerElement.addEventListener('click', function (evt) {
+      if (evt.target !== successInnerElement) {
+        closeSuccessMessage();
+      }
+    });
+    return successLayerElement;
+  };
+
+  var closeSuccessMessage = function () {
+    main.removeChild(successLayerElement);
+    document.removeEventListener('keydown', successMessageEscHandler);
+  };
+
+  var successMessageEscHandler = function (evt) {
+    window.utils.isEscEvent(evt, closeSuccessMessage);
+  };
+
+  var errorLayerElement = document.querySelector('.error');
+
+  var errorHandler = function (message) {
+    closeEditForm();
+    createResultMessage(errorTemplateElement, message);
+
+    errorLayerElement = document.querySelector('.error');
+    var messageButtonElement = errorLayerElement.querySelector('.error__button');
+    var errorInnerElement = errorLayerElement.querySelector('.error__inner');
+    document.addEventListener('keydown', errorMessageEscHandler);
+    messageButtonElement.addEventListener('click', function () {
+      closeErrorMessage();
+    });
+    errorLayerElement.addEventListener('click', function (evt) {
+      if (evt.target !== errorInnerElement) {
+        closeErrorMessage();
+      }
+    });
+    return errorLayerElement;
+  };
+
+  var closeErrorMessage = function () {
+    main.removeChild(errorLayerElement);
+    document.removeEventListener('keydown', errorMessageEscHandler);
+  };
+
+  var errorMessageEscHandler = function (evt) {
+    window.utils.isEscEvent(evt, closeErrorMessage);
+  };
+
+  var formSubmitHandler = function (evt) {
+    evt.preventDefault();
+    window.backend.save(new FormData(formElement), successHandler, errorHandler);
+  };
+
+  formElement.addEventListener('submit', formSubmitHandler);
 })();
